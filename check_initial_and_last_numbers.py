0053c108dfee48b7a8566a440f09791c75e02112
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 02:39:43 +0800

    Pasted the given list and added print to check the lists

--- check_initial_and_last_numbers.py
+++ check_initial_and_last_numbers.py
@@ -4,12 +4,19 @@
 
 # Create a function to define the initial and last number are match
     # If length of lst > 0:
+
+
     # Return True if initial and last is match
     # Else, return false
 
 # Paste the given first list
+first_list = [10, 20, 30, 40, 10]
+
     # Print first list
+print(first_list)
 
 # Paste the given second list
+second_list = [75, 65, 35, 75, 30]
     # Print second list
+print(second_list)
 
