760ea9c8107b4d97869bebbb4cd7ee1196b18cb2
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 02:31:22 +0800

    Created pseudocode

--- check_initial_and_last_numbers.py
+++ check_initial_and_last_numbers.py
@@ -1,1 +1,15 @@
 # Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+# Pseudocode
+
+# Create a function to define the initial and last number are match
+    # If length of lst > 0:
+    # Return True if initial and last is match
+    # Else, return false
+
+# Paste the given first list
+    # Print first list
+
+# Paste the given second list
+    # Print second list
+
