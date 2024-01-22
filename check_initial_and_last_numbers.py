7273baae76720b52147939615ae5a5f6dcc5274b
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 02:51:27 +0800

    Created the function for defining the number match

--- check_initial_and_last_numbers.py
+++ check_initial_and_last_numbers.py
@@ -5,9 +5,12 @@
 # Create a function to define the initial and last number are match
     # If length of lst > 0:
 def first_and_second_match(lst):
-
+    if len(lst)>0:
     # Return True if initial and last is match
+         return lst[0] == lst [-1]
     # Else, return false
+    else:
+         return False
 
 # Paste the given first list
 first_list = [10, 20, 30, 40, 10]
@@ -16,7 +19,7 @@ first_list = [10, 20, 30, 40, 10]
 result1 = first_and_second_match(first_list )
 
     # Print first list
-print("First Print Result is ", result1)
+print("First Result is ", result1)
 
 # Paste the given second list
 second_list = [75, 65, 35, 75, 30]
@@ -24,5 +27,5 @@ second_list = [75, 65, 35, 75, 30]
 result2 = first_and_second_match(second_list )
 
     # Print second list
-print("Second Result is :", result2)
+print("Second Result is ", result2)
 
